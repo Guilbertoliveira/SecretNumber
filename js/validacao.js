9accba2bbffa4b7146f5411382e4f69958e2311d
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-06 16:40:54 -0300

    Add files via upload

--- js/validacao.js
+++ js/validacao.js
@@ -1,3 +1,6 @@
+let pontos = document.getElementById('chances');
+
+
 function verificaValido(chute)
 {
     const numero = parseInt(chute);
@@ -25,20 +28,25 @@ function verificaValido(chute)
 
    if (numero === numeroSecreto) {
     document.body.innerHTML = `
+    <body><header><div class="chances"><p>Errou:</p><p id="chances">0</p></div></header>
+    <main>
     <h2 class="acertou">você acertou</h2>
     <h3 class="acertou">Numero secreto era ${numeroSecreto}</h3>
-    <button id="buttonStart">Começar Dnv</button>
+    <button id="buttonStart">Começar Dnv</button></main><body>
     `
-   
-
+    console.log(ponto);
    } else if (numero < numeroSecreto) {
     elementoChute.innerHTML += `
      <div>O numero secreto é maior <i class="fa-solid fa-turn-up"></i></div>
     `
+    return pontos.textContent = parseInt (pontos.textContent) + 1;
+
    } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
     <div>O numero secreto é menor <i class="fa-solid fa-turn-down"></i></div>
    `
+   return pontos.textContent = parseInt (pontos.textContent) + 1;
+
    }
 }
 
