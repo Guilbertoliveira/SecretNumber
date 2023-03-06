5ff9890f64a590decbf8e84e92961779432bec8d
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-05 21:23:24 -0300

    update code js

--- js/validacao.js
+++ js/validacao.js
@@ -27,7 +27,10 @@ function verificaValido(chute)
     document.body.innerHTML = `
     <h2 class="acertou">você acertou</h2>
     <h3 class="acertou">Numero secreto era ${numeroSecreto}</h3>
+    <button id="buttonStart">Começar Dnv</button>
     `
+   
+
    } else if (numero < numeroSecreto) {
     elementoChute.innerHTML += `
      <div>O numero secreto é maior <i class="fa-solid fa-turn-up"></i></div>
@@ -42,3 +45,12 @@ function verificaValido(chute)
 function maioroumenor (numero) {
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', botao=> {
+    if (botao.target.id == 'buttonStart') {
+        window.location.reload()
+    }
+})
+
+
+
